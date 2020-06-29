ed12fbe3c30790cd8630f3f409a609a40f8e3142
nullbitcoder <lxxxl1146@gmail.com>
2020-06-28 22:04:51 -0500

    Setting upServer 02

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Helloooo Thereeee');
+  //   res.send('Helloooo Thereee');
+  res.render('index');
 });
 
 app.listen(5000);
