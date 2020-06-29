828061066c9ada3b0d930820478971a81b797ed6
nullbitcoder <lxxxl1146@gmail.com>
2020-06-29 15:45:10 -0500

    Setting up Server 03, plus page rendering

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articleRouter); //path for browser
+
 app.get('/', (req, res) => {
   //   res.send('Helloooo Thereee');
-  res.render('index');
+  const articles = [
+    {
+      title: 'Test Article',
+      createdAt: new Date(),
+      description: 'Test description',
+    },
+    {
+      title: 'Test Article 2',
+      createdAt: new Date(),
+      description: 'Test description 2',
+    },
+  ];
+  res.render('index', { articles: articles });
 });
 
 app.listen(5000);
